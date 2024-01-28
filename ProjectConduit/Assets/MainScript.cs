e49864a7b59e2d0204b85aed2608065b4d010924
DavidGao0518 <dgao2007.primary@gmail.com>
2024-01-27 19:16:53 -0800

    sdffsf

--- ProjectConduit/Assets/MainScript.cs
+++ ProjectConduit/Assets/MainScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MainScript : MonoBehaviour
 {
 
-    public GameObject wire;
     // Start is called before the first frame update
     void Start()
     {
--- ProjectConduit/Assets/WireScript.cs
+++ ProjectConduit/Assets/WireScript.cs
@@ -2,14 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockScript : MonoBehaviour
+public class WireScript : MonoBehaviour
 {
-    public int inputAmount;
-
-
-    public Dictionary<int, GameObject> inputPorts;
-
-    public GameObject outputPort;
+    public GameObject attachment1;
+    public GameObject attachment2;
 
     // Start is called before the first frame update
     void Start()
