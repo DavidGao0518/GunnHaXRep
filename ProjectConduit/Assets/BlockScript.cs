7fa2e678bfa7322b79c7241e76c5d3fcfd8e489a
DavidGao0518 <dgao2007.primary@gmail.com>
2024-01-27 18:59:57 -0800

    ad

---
+++ ProjectConduit/Assets/BlockScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public int inputAmount;
+    public Dictionary<int, GameObject> inputPorts;
+
+    public GameObject outputPort;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectConduit/Assets/CameraScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 moveInputs;
+    public float moveSpeed;
+    float zoomInputs;
+    Camera currentCamera;
+    float zoomLevel;
+    public float zoomSpeed;
+    void Start()
+    {
+        currentCamera = transform.GetComponent<Camera>();
+        zoomLevel = currentCamera.orthographicSize;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3)moveInputs * Time.deltaTime * moveSpeed;
+
+        zoomLevel -= zoomInputs * zoomSpeed * Time.deltaTime;
+        currentCamera.orthographicSize = math.max(1, zoomLevel);
+        //print(moveInputs);
+    }
+
+    void OnMove(InputValue action)
+    {
+        moveInputs = action.Get<Vector2>();
+        //transform.position = (Vector2) transform.position + moveInputs;
+    }
+
+    void OnZoom(InputValue action)
+    {
+        zoomInputs = action.Get<float>();
+    }
+
+    void OnMouseMove(InputValue action)
+    {
+        float test = action.Get<float>();
+    }
+}
